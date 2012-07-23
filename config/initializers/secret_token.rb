69594834875968bbb9a838787e11d592ed5b4af2
June Jung <jeongjueun@gmail.com>
2012-07-23 10:43:33 -0700

    IT IS A BIG MOMENT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hnclone::Application.config.secret_token = '66e552c5575bb98b307e0537014b487ea6236dbc260a813d9b3f16f85dab48ad32fd88b56a0736c3f87ecf8ce68b8b43a6a21c2c6760ac37d1d6293f6d5723f4'
