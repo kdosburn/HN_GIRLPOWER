62eb03d5fd542f71d4f6b3db5f08eba917f8b616
Kendra Osburn + Jessie Young <apprentice+ko+jy@devbootcamp.com>
2012-07-26 20:56:39 -0700

    database clean out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726040746) do
+ActiveRecord::Schema.define(:version => 20120727035118) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(:version => 20120726040746) do
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
