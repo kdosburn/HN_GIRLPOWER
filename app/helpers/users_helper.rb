0fe8ee30451173d2f96aa304155d38127e873d23
June Jung <apprentice@devbootcamp.com>
2012-07-27 19:01:43 -0700

    gravatar fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   #Returns the Gravatar (http://gravatar.com/) for the given user.
 
   def gravatar_for(user)
-    if User.count > 0
+    if signed_in?
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
       image_tag(gravatar_url, alt: user.name, class: "gravatar")
